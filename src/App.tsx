b8e9c677254995d5207c0083d821db02cbf45d93
nostam <matsonho@gmail.com>
2021-03-17 00:33:53 +0100

    add ability to set stroke and color

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import Room from "./pages/Room";
 
 function App() {
   return (
     <BrowserRouter>
+      <Route path="/" component={NavBar} />
       <Route path="/" component={Room} />
     </BrowserRouter>
   );
--- src/components/Whiteboard.tsx
+++ src/components/Whiteboard.tsx
@@ -1,24 +1,75 @@
 import React, { useRef, useState, useCallback, useEffect } from "react";
 import io from "socket.io-client";
+import "../styles/Whiteboard.scss";
+import Slider from "@material-ui/core/Slider";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 
+interface IColor {
+  label: string;
+  value: string;
+}
 type Coordinate = { x: number; y: number };
 const socket = io(process.env.REACT_APP_API_URL!, {
   transports: ["websocket"],
 });
+
+const marks = [
+  {
+    value: 8,
+    label: "8px",
+  },
+  {
+    value: 16,
+    label: "16px",
+  },
+  {
+    value: 24,
+    label: "24px",
+  },
+  {
+    value: 96,
+    label: "96px",
+  },
+];
+
+const colors: IColor[] = [
+  { value: "#f44336", label: "Red" },
+  { value: "#e91e63", label: "Pink" },
+  { value: "#9c27b0", label: "Purple" },
+  { value: "#673ab7", label: "Deep Purple" },
+  { value: "#3f51b5", label: "Indigo" },
+  { value: "#2196f3", label: "Blue" },
+  { value: "#03a9f4", label: "Light Blue" },
+  { value: "#00bcd4", label: "Cyan" },
+  { value: "#fff", label: "White" },
+  { value: "#009688", label: "Teal" },
+  { value: "#4caf50", label: "Green" },
+  { value: "#8bc34a", label: "Light Green" },
+  { value: "#cddc39", label: "Lime" },
+  { value: "#ffeb3b", label: "Yellow" },
+  { value: "#ffc107", label: "Amber" },
+  { value: "#ff9800", label: "Orange" },
+  { value: "#ff5722", label: "Deep Orange" },
+  { value: "#000", label: "Black" },
+];
+
+function valuetext(value: number) {
+  return `${value}°C`;
+}
+
 function Whiteboard() {
-  const width = 1500;
-  const height = 1000;
+  const width = 10;
+  const height = 10;
   const [isDrawing, setIsDrawing] = useState<boolean>(false);
   const [mouseCoordinates, setMouseCoordinates] = useState<
     Coordinate | undefined
   >();
   const [color, setColor] = useState<string>("black");
-  const [stroke, setStroke] = useState<number>(12);
+  const [stroke, setStroke] = useState<number>(16);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const contextRef = useRef<CanvasRenderingContext2D | null>(null);
 
   useEffect(() => {
-    console.log("init canvas");
     const canvas = canvasRef.current!;
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -26,11 +77,8 @@ function Whiteboard() {
     canvas.style.height = `${window.innerHeight}px`;
 
     const context = canvas.getContext("2d")!;
-
     context.lineCap = "round";
     context.lineJoin = "round";
-    context.strokeStyle = color;
-    context.lineWidth = stroke;
     contextRef.current = context;
 
     //receiver side
@@ -40,6 +88,11 @@ function Whiteboard() {
       img.onload = () => context!.drawImage(img, 0, 0);
     });
   }, []);
+  useEffect(() => {
+    const context = canvasRef.current!.getContext("2d")!;
+    context.strokeStyle = color;
+    context.lineWidth = stroke;
+  }, [stroke, color]);
 
   useEffect(() => {
     const room = "test";
@@ -58,7 +111,7 @@ function Whiteboard() {
   function finishDrawing(e: MouseEvent) {
     contextRef.current!.closePath();
     setIsDrawing(false);
-    const dataURL = canvasRef.current!.toDataURL("image/webp", 0.75); // firefox not support
+    const dataURL = canvasRef.current!.toDataURL("image/webp", 0.75); // firefox does not support
     setTimeout(() => socket.emit("canvasData", dataURL), 500);
   }
   function draw(e: MouseEvent) {
@@ -71,13 +124,41 @@ function Whiteboard() {
   return (
     <div id="canvas">
       <canvas
+        id="canvas"
         ref={canvasRef}
-        height={height}
-        width={width}
         onMouseDown={(e) => startDrawing(e.nativeEvent)}
         onMouseUp={(e) => finishDrawing(e.nativeEvent)}
         onMouseMove={(e) => draw(e.nativeEvent)}
       />
+      <div id="drawingTools">
+        <div id="colorPalettes">
+          <div id="currentColor" style={{ backgroundColor: color }}></div>
+          <div id="palettes">
+            {colors.map((c) => (
+              <div
+                className="color"
+                key={c.label.trim()}
+                style={{ backgroundColor: c.value }}
+                onClick={() => setColor(c.value)}
+              />
+            ))}
+          </div>
+        </div>
+        <div id="brush">
+          <Slider
+            value={stroke}
+            getAriaValueText={valuetext}
+            aria-labelledby="discrete-slider-custom"
+            step={4}
+            valueLabelDisplay="auto"
+            marks={marks}
+            onChangeCommitted={(e, value) => {
+              console.log(typeof value === "number");
+              if (typeof value === "number") setStroke(value);
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
