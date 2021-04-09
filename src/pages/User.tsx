2f1e3813b9425cc3d408d67f7b99660fd2abcd54
nostam <matsonho@gmail.com>
2021-04-09 02:21:40 +0200

    update user pages and add point interface

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { Grid, Paper, Typography } from "@material-ui/core";
+import { useAppSelector } from "../utils/hooks";
+import "../styles/User.scss";
 
 export default function User() {
-  return <div></div>;
+  const { user } = useAppSelector((state) => state.user);
+  return (
+    <div id="user-wrapper">
+      <Grid container>
+        <Grid item xs={4} />
+        <Grid item xs={4}>
+          <Paper id="user-container" elevation={4}>
+            <div className="row userinfo">
+              <img src={user.avatar} alt="avatar" id="user-avatar" />
+              <Typography variant="h4">{`${user.username}`}</Typography>
+            </div>
+            <Typography
+              variant="h6"
+              id="userstat"
+            >{`Total Points : ${user.point}`}</Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={4} />
+      </Grid>
+    </div>
+  );
 }
