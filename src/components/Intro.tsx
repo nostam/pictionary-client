cdf2fccad7caa031c373fe2445b3011210bf5e73
nostam <matsonho@gmail.com>
2021-04-12 21:44:18 +0200

    add demo video

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -9,7 +9,25 @@ export default function Intro() {
         A word-guessing game with drawings
       </Typography>
       <br />
-      <div id="intro-video">demo video here</div>
+      <div id="intro-video">
+        {/* <Video publicId="demo/2021-04-12_21-15-10_v7fs9u">
+          <Transformation
+            audioCodec="none"
+            background="auto"
+            effect="accelerate:100"
+            quality="auto:good"
+            width="1280"
+            crop="scale"
+          />
+        </Video> */}
+
+        <video
+          style={{ display: "block" }}
+          width="100%"
+          autoPlay
+          src="https://res.cloudinary.com/dnii0enil/video/upload/ac_none,c_scale,e_accelerate:100,q_auto:good,w_1280/v1618255457/demo/2021-04-12_21-15-10_v7fs9u.mp4"
+        />
+      </div>
       <Typography variant="h6">
         Everyone can join and play without any regisration
       </Typography>
@@ -20,7 +38,7 @@ export default function Intro() {
           <li className="intro-list">
             Points - Player who drew or guessed correctly
           </li>
-          {/* <li className="intro-list">Ranking</li> */}
+          <li className="intro-list">Ranking</li>
           <li className="intro-list">Customize site theme color</li>
           {/* <li className="intro-list">Voting for best illustrations</li> */}
         </ul>
